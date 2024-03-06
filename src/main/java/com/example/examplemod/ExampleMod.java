bdf56790c2abe88357aa24ffe05dd9b6089af52e
Jeff <jeff.m.lieb@gmail.com>
2024-03-05 23:18:53 -0500

    Adventures in exploring how to detect bees entering the hive

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -3,6 +3,8 @@ package com.example.examplemod;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -10,12 +12,17 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.VanillaGameEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -28,6 +35,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import net.minecraftforge.event.entity.living.LivingEvent;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
@@ -77,9 +85,13 @@ public class ExampleMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockEvent);
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockEventEntityPlaceEvent);
+        MinecraftForge.EVENT_BUS.addListener(this::onLevelEvent);
+        MinecraftForge.EVENT_BUS.addListener(this::onVanillaGameEvent);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+//        MinecraftForge.EVENT_BUS.addListener(this::onBlockEvent);
+
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -98,19 +110,62 @@ public class ExampleMod
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    private void onBlockEvent(BlockEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        logEvent(event);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
+    private void onBlockEventEntityPlaceEvent(BlockEvent.EntityPlaceEvent event)
     {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        logEvent(event);
+    }
+
+    private void onLevelEvent(LevelEvent event)
+    {
+        if (event instanceof LevelEvent.PotentialSpawns) {
+            return;
+        }
+
+        logEvent(event);
+    }
+
+    private void onLivingEvent(LivingEvent event) {
+
+//        if (event.getEntity() instanceof Bee) {
+//            LOGGER.info("It's a bee!");
+//            Bee bee = (Bee) event.getEntity();
+//            bee.level().getBlockEntity(bee.)
+//        }
+//
+//        if (event.getEntity() instanceof Player) {
+//            return;
+//        }
+//        logEvent(event);
+    }
+
+    private void onVanillaGameEvent(VanillaGameEvent event) {
+        LOGGER.info("Got vanilla game event " + event.getVanillaEvent() + event.getContext());
+        LOGGER.info("Entity class " + event.getContext().sourceEntity().getClass().getName());
+
+
+
+        if (event.getVanillaEvent() == GameEvent.BLOCK_CHANGE &&
+            event.getContext().sourceEntity() instanceof Bee)
+        {
+            LOGGER.info("IT IS THE THINGS" + event.getCause() + ", " + event.getContext().sourceEntity());
+            BlockEntity derp = event.getLevel().getBlockEntity(event.getCause().blockPosition());
+            Bee bee = (Bee) event.getContext().sourceEntity();
+            BeehiveBlockEntity beehive = (BeehiveBlockEntity) event.getLevel().getBlockEntity(bee.getHivePos());
+            LOGGER.info("Here be bees " + beehive.serializeNBT().get("Bees"));
+
+            if (derp instanceof BeehiveBlockEntity) {
+                LOGGER.info("HEY IT'S A BEEHIVE " + derp.getBlockState());
+            }
+        }
+    }
+
+    private void logEvent(Event event) {
+        LOGGER.info("Got event" + event.getClass().getName());
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
@@ -125,4 +180,7 @@ public class ExampleMod
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
+
+
 }
+
